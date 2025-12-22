a7b2ca5a6e49c913841918b6b8a14afd33a64ed9
FDMSV <adv@hop.fr>
2025-12-22 13:57:11 +0100

    Create HOPcolors.js

---
+++ HOPcolors.js
@@ -0,0 +1,27 @@
+let couleur_AF = '#051039';
+let couleur_HOP = '#f00500';
+
+let couleur_HOP_CP1 = '#FFB500';
+let couleur_HOP_CP1_CA1 = '#FFCE00';
+let couleur_HOP_CP1_CA2 = '#FFE805';
+let couleur_HOP_CP1_CA3 = '#FF8811';
+
+let couleur_HOP_CP2 = '#7AB800';
+let couleur_HOP_CP2_CA1 = '#B9D300';
+let couleur_HOP_CP2_CA2 = '#E1E400';
+let couleur_HOP_CP2_CA3 = '#55BE47';
+
+let couleur_HOP_CP3 = '#00B09D';
+let couleur_HOP_CP3_CA1 = '#00BAB9';
+let couleur_HOP_CP3_CA2 = '#3FB6D2';
+let couleur_HOP_CP3_CA3 = '#00AE68';
+
+let couleur_HOP_CP4 = '#0061CE';
+let couleur_HOP_CP4_CA1 = '#2986E8';
+let couleur_HOP_CP4_CA2 = '#00A7E3';
+let couleur_HOP_CP4_CA3 = '#4242D0';
+
+let couleur_HOP_CP5 = '#793AAD';
+let couleur_HOP_CP5_CA1 = '#962399';
+let couleur_HOP_CP5_CA2 = '#BB23A0';
+let couleur_HOP_CP5_CA3 = '#6023C2';
