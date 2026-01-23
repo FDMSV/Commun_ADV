d1ce5029575fb488b0d09c5ba6304014f09402d4
FDMSV <adv@hop.fr>
2026-01-23 14:46:23 +0100

    Update colors.js

--- colors.js
+++ colors.js
@@ -3,7 +3,8 @@ window.HOP_COLORS = {
   couleur_AF: '#051039',
   couleur_HOP: '#f00500',
   blanc: '#ffffff',
-  gris-clair : '#f4f4f4',
+  gris_clair : '#f4f4f4',
+  rouge_clair = '#f08888',
 
   couleur_HOP_CP1: '#FFB500',
   couleur_HOP_CP1_CA1: '#FFCE00',
